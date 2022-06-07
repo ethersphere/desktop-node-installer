19991b6636c4c9171812c2a9e3f3aa11da5363c1
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2022-06-07 11:30:26 +0200

    feat: add installer design (#2)
    * feat: add designed installer
    
    * fix: add missing net.js
    
    * feat: add loading screen
    
    * fix: correct spelling (#3)
    
    Just a small typo <3
    
    Co-authored-by: Michelle Plur <52613476+michelleplur@users.noreply.github.com>

--- src/Typography.jsx
+++
@@ -1,8 +0,0 @@
-export function Typography({ size = '15px', color = '#000', children }) {
-    const style = {
-        fontSize: size,
-        color
-    }
-
-    return <p style={style}>{children}</p>
-}
