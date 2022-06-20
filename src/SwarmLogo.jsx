a3c2a4ca99f4c1b3be1269c38d58771c2fb07cf2
Cafe137 <xcafe137@gmail.com>
2022-06-20 11:42:38 +0200

    feat: update swarm desktop logo

--- src/SwarmLogo.jsx
+++ src/SwarmLogo.jsx
@@ -1,7 +1,7 @@
 export function SwarmLogo() {
     return (
         <div>
-            <img className="logo" alt="Swarm Dashboard Logo" src="/installer/swarm-dashboard-logo.png" />
+            <img className="logo" alt="Swarm Dashboard Logo" src="/installer/swarm-desktop-logo.svg" />
         </div>
     )
 }
